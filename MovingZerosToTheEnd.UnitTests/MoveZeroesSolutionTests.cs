bf249b0e0091d3b00a9df8a37aafbe6551b98832
DAkbariSE <danial_akbari33@hotmail.com>
2023-08-02 14:00:15 +0200

    Updating MoveZeroesToTheEnd Test Cases

--- MovingZerosToTheEnd.UnitTests/MoveZeroesSolutionTests.cs
+++ MovingZerosToTheEnd.UnitTests/MoveZeroesSolutionTests.cs
@@ -4,7 +4,7 @@ namespace MovingZerosToTheEnd.UnitTests
     public class MoveZeroesSolutionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MoveZeroes_PositiveNumbers_ShouldBeOrderedCorrectly()
         {
             var response = MoveZeroesSolution.MoveZeroes(
                     new int[]
@@ -12,6 +12,24 @@ namespace MovingZerosToTheEnd.UnitTests
                         1, 2, 0, 1, 0, 1, 0, 3, 0, 1
                     });
             CollectionAssert.AreEqual(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }, response);
+        }
+
+        [TestMethod]
+        public void MoveZeroes_WithNegativeNumbers_ShouldBeOrderedCorrectly()
+        {
+            var response = MoveZeroesSolution.MoveZeroes(
+                    new int[]
+                    {
+                        1, -2, 0, -1, 0, 1, 0, 3, 0, 1
+                    });
+            CollectionAssert.AreEqual(new int[] { 1, -2, 1, -1, 3, 1, 0, 0, 0, 0 }, response);
+        }
+
+        [TestMethod]
+        public void MoveZeroes_EmptyList_ShouldReturnEmptyList()
+        {
+            var response = MoveZeroesSolution.MoveZeroes(new int[] {});
+            CollectionAssert.AreEqual(new int[] {}, response);
         }
     }
 }
