1662d51674391a2456599fc007df3f161236621a
DAkbariSE <danial_akbari33@hotmail.com>
2023-08-03 13:42:43 +0200

    Add ReplaceWithAlphabetPosition Challenge

---
+++ ReplaceWithAlphabetPosition/ReplaceWithAlphabetPositionSolution.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace ReplaceWithAlphabetPosition
+{
+    public class ReplaceWithAlphabetPositionSolution
+    {
+        public static string AlphabetPosition(string text)
+        {
+            return "";
+        }
+    }
+}
